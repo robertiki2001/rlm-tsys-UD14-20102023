45102c14287adadfc6f4969560fcb4b904ea6eff
Robert <95480383+robertiki2001@users.noreply.github.com>
2023-10-20 19:55:51 +0200

    Inserts, Updates, Deletes

--- ej8.sql
+++ ej8.sql
@@ -19,3 +19,58 @@ FOREIGN KEY (codigo_pieza) REFERENCES Piezas(codigo) ON DELETE CASCADE ON UPDATE
 FOREIGN KEY (id_proveedor) REFERENCES Proveedores(id) ON DELETE CASCADE ON UPDATE CASCADE,
 PRIMARY KEY (codigo_pieza, id_proveedor)
 );
+
+-- -------------------------INSERTS--------------------------------ç
+INSERT INTO Piezas (nombre) VALUES
+('Pieza A'),
+('Pieza B'),
+('Pieza C'),
+('Pieza D'),
+('Pieza E'),
+('Pieza F'),
+('Pieza G'),
+('Pieza H'),
+('Pieza I'),
+('Pieza J');
+
+INSERT INTO Proveedores (id, nombre) VALUES
+('P1', 'Proveedor 1'),
+('P2', 'Proveedor 2'),
+('P3', 'Proveedor 3'),
+('P4', 'Proveedor 4'),
+('P5', 'Proveedor 5'),
+('P6', 'Proveedor 6'),
+('P7', 'Proveedor 7'),
+('P8', 'Proveedor 8'),
+('P9', 'Proveedor 9'),
+('P10', 'Proveedor 10');
+
+INSERT INTO Suministra (codigo_pieza, id_proveedor, precio) VALUES
+(1, 'P1', 100),
+(2, 'P2', 150),
+(3, 'P3', 80),
+(4, 'P4', 120),
+(5, 'P5', 200),
+(6, 'P6', 90),
+(7, 'P7', 180),
+(8, 'P8', 110),
+(9, 'P9', 130),
+(10, 'P10', 70);
+-- -------------------------UPDATES--------------------------------
+UPDATE Piezas SET nombre = 'Nueva Pieza A' WHERE codigo = 1;
+UPDATE Piezas SET nombre = 'Nueva Pieza B' WHERE codigo = 2;
+
+UPDATE Proveedores SET nombre = 'Nuevo Proveedor 1' WHERE id = 'P1';
+UPDATE Proveedores SET nombre = 'Nuevo Proveedor 2' WHERE id = 'P2';
+
+UPDATE Suministra SET precio = 120 WHERE codigo_pieza = 1 AND id_proveedor = 'P1';
+UPDATE Suministra SET precio = 160 WHERE codigo_pieza = 2 AND id_proveedor = 'P2';
+-- -------------------------DELETES--------------------------------
+DELETE FROM Piezas WHERE codigo = 3;
+DELETE FROM Piezas WHERE codigo = 4;
+
+DELETE FROM Proveedores WHERE id = 'P3';
+DELETE FROM Proveedores WHERE id = 'P4';
+
+DELETE FROM Suministra WHERE codigo_pieza = 3 AND id_proveedor = 'P3';
+DELETE FROM Suministra WHERE codigo_pieza = 4 AND id_proveedor = 'P4';
